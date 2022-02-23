5e8a61add07e9689276dce58eb012394a20d644e
sanghunDev <bsh20057@gmail.com>
2022-02-23 20:23:31 +0900

    add : 11718 init

---
+++ src/io/boj11718/Main.java
@@ -0,0 +1,19 @@
+package io.boj11718;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str = "";
+
+        while((str = buf.readLine()) != null){
+            sb.append(str);
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
