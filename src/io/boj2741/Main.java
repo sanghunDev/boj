b77fd9dd06ffc9eca02edb19418f9521814b1f46
sanghun <bsh20057@gmail.com>
2022-02-27 23:24:03 +0900

    add : 2741 init

---
+++ src/io/boj2741/Main.java
@@ -0,0 +1,16 @@
+package io.boj2741;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+        int loopLeng = Integer.parseInt(buf.readLine());
+
+        for (int i = 1; i <= loopLeng; i++) {
+            System.out.println(i);
+        }
+    }
+}
