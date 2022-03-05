ffb5bc293eb2e3c32f964c4cfcf151b441d9ade0
sanghun <bsh20057@gmail.com>
2022-03-05 23:10:15 +0000

    add : 2438 init

---
+++ src/io/boj2438/Main.java
@@ -0,0 +1,19 @@
+package io.boj2438;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+        int loopLeng = Integer.parseInt(buf.readLine());
+
+        for (int i = 0; i < loopLeng; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
