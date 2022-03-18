e88ea64a529475f573b54a147ff9da972dc212c9
sanghun <bsh20057@gmail.com>
2022-03-18 23:43:12 +0000

    add : 2743 init

---
+++ src/io/boj2743/Main.java
@@ -0,0 +1,14 @@
+package io.boj2743;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+        String s = buf.readLine();
+
+        System.out.println(s.length());
+    }
+}
